66ad6cce055d5661d7bbb1b209bb37cc05d7cca5
Manuel Emiliano Aguado Meza <manuelaguado@gmail.com>
2016-12-03 04:17:11 -0600

    cambios sql

--- mysql_changes.sql
+++ mysql_changes.sql
@@ -1,15 +1,5 @@
 -- Cambios para produccion
-set FOREIGN_KEY_CHECKS=0;
-
-ALTER TABLE `centralcar`.`cl_tarifas_clientes` ADD COLUMN `tabulado` int(1) NULL AFTER `cat_tipo_tarifa`;
-
-set FOREIGN_KEY_CHECKS=1;
 
 
 
 -- Cambios para cloud9
-set FOREIGN_KEY_CHECKS=0;
-
-ALTER TABLE `centralcar`.`cl_tarifas_clientes` ADD COLUMN `tabulado` int(1) NULL AFTER `cat_tipo_tarifa`;
-
-set FOREIGN_KEY_CHECKS=1;
