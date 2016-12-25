481141f961cc333b4712d77097051bcc0466ec23
Manuel Emiliano Aguado Meza <manuelaguado@gmail.com>
2016-12-25 16:03:07 -0600

    evitar la recarga del dom para el mobil

--- public/fw7/assets/js/app.js
+++ public/fw7/assets/js/app.js
@@ -67,9 +67,10 @@ $$("body").on("click", ".select_car", function() {
 		dataType: "json",
 		success: function(respuesta){
 			if (respuesta['resp'] == true) {
-				initEpisodioAuto();
-				var mvhc = 1;
-				window.location ="mobile";
+				storeClave('C1','C1','F11','NULL','NULL','NULL',function(){
+					var mvhc = 1;
+					loadTemplateWOR1('regreso');
+				});
 			}else{
 				myApp.alert('Intente seleccionar la unidad cuando tenga datos APP1', 'Sin conexión');
 			}
