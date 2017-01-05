2397b51bf081eef6186aa1e64c48e9071669a78d
Manuel Emiliano Aguado Meza <manuelaguado@gmail.com>
2017-01-04 22:33:59 -0600

    token truncate

--- modelos/mobile.php
+++ modelos/mobile.php
@@ -12,11 +12,13 @@ class MobileModel
     }
 	function store($claves, OperacionModel $operacion, $tknses){
 		$output[0] = array('resp' => false);
+		foreach($claves as $num => $clave){
+			
 		error_log('Op>> '.$clave['id_operador']);
 		error_log('Cve>> '.$clave['clave']);
 		error_log('Tkn>> '.$clave['token']);
-		error_log('tknses>> '.$tknses);
-		foreach($claves as $num => $clave){
+		error_log('tknses>> '.$tknses);			
+			
 			$tokenStore = self::tokenStore($clave['token']);
 			error_log('tokenStore>> '.$tokenStore);
 			if($tokenStore == 0){
