f3727301e0de53d0ee591c513d5ff91e729b0aef
Manuel Emiliano Aguado Meza <manuelaguado@gmail.com>
2017-01-08 06:25:25 -0600

    Modificando intervals

--- controladores/mobile.php
+++ controladores/mobile.php
@@ -30,7 +30,6 @@ class Mobile extends Controlador
 		}
     }
 	public function pusher_auth(){
-		header('Access-Control-Allow-Origin: *');
 		require_once('../vendor/pusher/Pusher.php');
 		$pusher = new Pusher(PUSHER_KEY, PUSHER_SECRET, PUSHER_APP_ID);
 		$presence_data = array(
